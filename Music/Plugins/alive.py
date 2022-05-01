332cb3363be8e1b50dc76fd0b09eb01488328af6
Githupxxx <104344083+Githupxxx@users.noreply.github.com>
2022-05-02 05:04:22 +0700

    Update alive.py

--- Music/Plugins/alive.py
+++ Music/Plugins/alive.py
@@ -42,7 +42,7 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-KYY_IMG = "https://telegra.ph/file/2b610c918dec590d2777b.jpg"
+KYY_IMG = "https://telegra.ph/file/88b54a898377c8b05ead1.jpg"
 
 
 @app.on_message(filters.command(["alive", "alive@Tg_Vc_00_Bot"]))
@@ -58,7 +58,7 @@ async def alive(client, message):
 ✘ **Pyrogram Version : `{pyrover}`** \n
 ✘ **PyTgCalls Version: `{pytover.__version__}`** \n
 ✘ **Using New Version** \n
-**Thanks For Using Me 🔥**""",
+**Thanks For Using Me Dan makasih buat Cimon ganteng  🔥**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
